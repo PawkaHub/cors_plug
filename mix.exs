f452d14e2daf35631fb85b83849251ef714ae5a1
PawkaHub <pieterjparker@gmail.com>
2017-12-17 19:29:31 -0700

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CorsPlug.Mixfile do
   def project do
     [
       app: :cors_plug,
-      version: "1.5.0",
+      version: "1.5.1",
       elixir: "~> 1.3",
       deps: deps(),
       package: package(),
