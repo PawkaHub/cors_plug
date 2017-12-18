0f68a84113f3974e226c770b5539bc5d1e3eba10
PawkaHub <pieterjparker@gmail.com>
2017-12-17 19:35:42 -0700

    Made plug dependency overridable

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule CorsPlug.Mixfile do
 
   defp deps do
     [
-      {:plug, "~> 1.4 or ~> 1.5", optional: true},
+      {:plug, "~> 1.4 or ~> 1.5", override: true},
 
       {:ex_doc, "~> 0.11", only: :dev},
       {:earmark, "~> 1.2", only: :dev},
